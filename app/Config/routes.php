86f7586557c8ac9bb5dcd9a3dfcc349ce4524644
aiiro <akairo29@gmail.com>
2016-04-26 00:01:41 +0900

    新規投稿、記事一覧のルーティングを追加

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -16,11 +16,11 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+  Router::connect('/blogs/new', ['controller' => 'posts', 'action' => 'add']);
+  Router::connect('/:user_account/blog/*', ['controller' => 'posts', 'action' => 'index'], ['user_account']);
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on
